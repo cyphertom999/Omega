1e5237925807205110f2095d7ec1b334e4a8457e
cyphertom999 <69469900+cyphertom999@users.noreply.github.com>
2020-08-10 20:12:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB7KY7XRJKLZSyNEQFmL3YnRdPfMcrBiZE",
+  authDomain: "confessout-44954.firebaseapp.com",
+  databaseURL: "https://confessout-44954.firebaseio.com",
+  projectId: "confessout-44954",
+  storageBucket: "confessout-44954.appspot.com",
+  messagingSenderId: "1049658064811",
+  appId: "1:1049658064811:web:04bfb694a462fe6144dcee",
+  measurementId: "G-MTR494LQDN"
 };
 
      
